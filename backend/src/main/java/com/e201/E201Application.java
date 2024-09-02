738e61af493fb8bfa5354f86c3588f0184a91442
이준혁 <jhl221123@naver.com>
2024-09-02 09:22:02 +0900

    Initial commit

---
+++ backend/src/main/java/com/e201/E201Application.java
@@ -0,0 +1,13 @@
+package com.e201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E201Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/e201/E201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
