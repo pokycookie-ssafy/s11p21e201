8705ef8a0d8654181725f209a868c652baea631b
황세웅 <0cookieboy0@gmail.com>
2024-09-11 18:00:04 +0900

    [#7] feat: react-hook-form 적용

--- frontend/packages/store/src/pages/auth/sign-up-form-view.tsx
+++ frontend/packages/store/src/pages/auth/sign-up-form-view.tsx
@@ -1,15 +1,41 @@
-import type { ISignUpResponse } from '@/types/sign-up'
+import type { IBankResponse, ISignUpResponse } from '@/types/sign-up'
 
 import api from '@/configs/api'
 import axios from '@/configs/axios'
 import { useTranslate } from '@/locales'
+import { useForm } from 'react-hook-form'
 import { useMemo, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
+import { FormInput } from '@/components/form/form-input'
 import BusinessLicenseForm from '@/sections/sign-up/business-license-form'
 
-import { Box, Stack, Button, useTheme, TextField, Typography, useMediaQuery } from '@mui/material'
+import {
+  Box,
+  Stack,
+  Button,
+  useTheme,
+  MenuItem,
+  Typography,
+  useMediaQuery,
+  CircularProgress,
+} from '@mui/material'
 
 import { Upload } from '@e201/ui'
+
+interface IForm {
+  email: string
+  password: string
+  passwordConfirm: string
+  phone: string
+  bank: string
+  account: string
+  companyName: string
+  repName: string
+  address: string
+  registerNumber: string
+  type: string
+  openDate: string
+}
 
 interface IProps {
   onNext: () => void
@@ -22,6 +48,25 @@ export default function SignUpFormView({ onNext }: IProps) {
   const stackDirection = useMediaQuery(breakpoints.up('md')) ? 'row' : 'column'
 
   const [file, setFile] = useState<File | null>(null)
+
+  const formMethod = useForm<IForm>({
+    mode: 'onSubmit',
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      phone: '',
+      bank: '',
+      account: '',
+      companyName: '',
+      repName: '',
+      address: '',
+      registerNumber: '',
+      type: '',
+      openDate: '',
+    },
+  })
+  const { control, watch } = formMethod
 
   const queryFn = async () => {
     const response = await axios.post<ISignUpResponse>(api.signUp.ocr, {})
@@ -40,6 +85,22 @@ export default function SignUpFormView({ onNext }: IProps) {
     gcTime: Infinity,
   })
 
+  const queryFn2 = async () => {
+    const response = await axios.get<IBankResponse[]>(api.signUp.bank)
+    return response.data
+  }
+
+  const {
+    data: bankData,
+    isPending: bankPending,
+    isError: bankError,
+  } = useQuery({
+    queryKey: [api.signUp.bank],
+    queryFn: queryFn2,
+    staleTime: Infinity,
+    gcTime: Infinity,
+  })
+
   const fileChangeHandler = (files: File[]) => {
     if (files.length === 0) {
       setFile(null)
@@ -52,6 +113,28 @@ export default function SignUpFormView({ onNext }: IProps) {
     onNext()
   }
 
+  const BankDataOptions = useMemo(() => {
+    if (bankPending) {
+      return (
+        <Stack p={0.5} justifyContent="center" alignItems="center">
+          <CircularProgress size={20} />
+        </Stack>
+      )
+    }
+    if (bankError) {
+      return (
+        <Stack p={0.5} justifyContent="center" alignItems="center">
+          <Typography>{t('error.bank')}</Typography>
+        </Stack>
+      )
+    }
+    return bankData.map((option, i) => (
+      <MenuItem key={i} value={option.bankCode}>
+        {option.bankName}
+      </MenuItem>
+    ))
+  }, [bankData, bankError, bankPending, t])
+
   const BusinessLicenseFormRender = useMemo(() => {
     if (!file) {
       return null
@@ -63,36 +146,84 @@ export default function SignUpFormView({ onNext }: IProps) {
   }, [isError, licenseData, file, isPending])
 
   return (
-    <Stack spacing={4}>
-      <Stack spacing={4} direction={stackDirection}>
-        <Stack spacing={1} width={1} sx={{ mb: 2 }}>
-          <Typography variant="subtitle1" sx={{ pl: 1, pb: 1 }}>
-            {t('title.user')}
-          </Typography>
-          <TextField fullWidth label={t('form.email')} size="small" />
-          <TextField fullWidth label={t('form.password')} size="small" type="password" />
-          <TextField fullWidth label={t('form.password_confirm')} size="small" type="password" />
-          <TextField fullWidth label={t('form.phone')} size="small" type="tel" />
+    <form onSubmit={formMethod.handleSubmit(submitHandler)} noValidate>
+      <Stack spacing={4}>
+        <Stack spacing={4} direction={stackDirection}>
+          <Stack spacing={2} width={1} sx={{ mb: 2 }}>
+            <Typography variant="subtitle1" sx={{ pl: 1 }}>
+              {t('title.user')}
+            </Typography>
+            <FormInput
+              name="email"
+              control={control}
+              rules={{
+                required: t('validate.email.required'),
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: t('validate.email.pattern'),
+                },
+              }}
+              label={t('form.email')}
+              type="email"
+            />
+            <FormInput
+              name="password"
+              control={control}
+              rules={{
+                required: t('validate.password.required'),
+                minLength: {
+                  value: 8,
+                  message: t('validate.password.minLength'),
+                },
+              }}
+              label={t('form.password')}
+              type="password"
+            />
+            <FormInput
+              name="passwordConfirm"
+              control={control}
+              rules={{
+                required: t('validate.password_confirm.validate'),
+                validate: {
+                  confirm: (value) =>
+                    value === watch('password') || t('validate.password_confirm.validate'),
+                },
+              }}
+              label={t('form.password_confirm')}
+              type="password"
+            />
+            <FormInput
+              name="phone"
+              control={control}
+              rules={{
+                required: t('validate.phone.required'),
+              }}
+              label={t('form.phone')}
+              type="tel"
+            />
+          </Stack>
+
+          <Stack spacing={2} width={1} sx={{ mb: 2 }}>
+            <Typography variant="subtitle1" sx={{ pl: 1 }}>
+              {t('title.account')}
+            </Typography>
+            <FormInput name="bank" control={control} label={t('form.bank')} select>
+              {BankDataOptions}
+            </FormInput>
+            <FormInput name="account" control={control} label={t('form.account')} />
+          </Stack>
         </Stack>
 
-        <Stack spacing={1} width={1} sx={{ mb: 2 }}>
-          <Typography variant="subtitle1" sx={{ pl: 1, pb: 1 }}>
-            {t('title.account')}
+        <Stack spacing={2} width={1} sx={{ mb: 2 }}>
+          <Typography variant="subtitle1" sx={{ pl: 1 }}>
+            {t('title.business')}
           </Typography>
-          <TextField fullWidth label={t('form.bank')} size="small" />
-          <TextField fullWidth label={t('form.account')} size="small" />
+          <Upload placeholder={t('form.upload')} onChange={fileChangeHandler} />
+          {BusinessLicenseFormRender}
         </Stack>
-      </Stack>
 
-      <Stack spacing={1} width={1} sx={{ mb: 2 }}>
-        <Typography variant="subtitle1" sx={{ pl: 1, pb: 1 }}>
-          {t('title.business')}
-        </Typography>
-        <Upload placeholder={t('form.upload')} onChange={fileChangeHandler} />
-        {BusinessLicenseFormRender}
+        <Button type="submit">{t('button.next')}</Button>
       </Stack>
-
-      <Button onClick={submitHandler}>{t('button.next')}</Button>
-    </Stack>
+    </form>
   )
 }
