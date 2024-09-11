9c73249e879cd62d321724b832a891d5ba5a98fa
이준혁 <jhl221123@naver.com>
2024-09-11 11:57:35 +0900

    [#2] fix: 직원 도메인이 기업 도메인 하위에 위치하도록 수정

--- backend/src/main/java/com/e201/domain/repository/company/EmployeeRepository.java
+++ backend/src/main/java/com/e201/domain/repository/company/EmployeeRepository.java
@@ -1,9 +1,8 @@
-package com.e201.domain.repository.employee;
+package com.e201.domain.repository.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.e201.domain.entity.company.Company;
-import com.e201.domain.entity.employee.Employee;
+import com.e201.domain.entity.company.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
