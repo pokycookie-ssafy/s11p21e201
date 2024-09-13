6816b245c24d27e15d9ca7356b9588b9bd9b51c4
황세웅 <0cookieboy0@gmail.com>
2024-09-13 09:39:19 +0900

    [#15] fix: mirage 서버 설정 변경

---
+++ frontend/packages/store/src/main.tsx
@@ -0,0 +1,20 @@
+import '@/global.css'
+
+import '@/locales/i18n'
+import { StrictMode } from 'react'
+import initServer from '@/mock-api/server'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+
+import router from './routes/router'
+
+initServer()
+// if (import.meta.env.DEV && import.meta.env.VITE_MOCK_API === 'MOCK') {
+//   initServer()
+// }
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
