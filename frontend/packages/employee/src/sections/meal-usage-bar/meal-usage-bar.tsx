5e2bc1a3ce049fc4102893b82d35e5fa44c0bf3c
김송희 <kkbuh2@gmail.com>
2024-09-20 10:18:17 +0900

    [#9] refactor: employee - 메인, QR 페이지 관련 코코드 개선

--- frontend/packages/employee/src/sections/meal-usage-bar/meal-usage-bar.tsx
+++ frontend/packages/employee/src/sections/meal-usage-bar/meal-usage-bar.tsx
@@ -1,66 +1,72 @@
+import { useTranslate } from '@/locales'
 import { useEmployeeMeal } from '@/hooks/api'
 
-import { Box, Typography } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge'
 
 export function MealUsageBar() {
+  const { t } = useTranslate('common')
+
   const id = 'e7ce2fe0-bf18-408e-b3ea-cb45fa46a469'
   const { data: meal, isLoading, error } = useEmployeeMeal(id)
 
   if (isLoading) return <Typography variant="h5">Loading...</Typography>
   if (error) return <Typography variant="h5">Error: {error.message}</Typography>
 
+  const currentMonth = new Date().getMonth() + 1
+  const currentYear = new Date().getFullYear()
+
+  // i18n 파일에서 정의된 텍스트(value)에 month, year 변수를 삽입
+  const title = t('current.title', { month: currentMonth, year: currentYear })
+
   // 데이터를 받아온 후 식대 사용 현황 계산
   const totalAmount = meal?.monthlyAmount ?? 0
   const currentUsage = meal?.currentUsage ?? 0
-  const remainingAmount = totalAmount - currentUsage
-  const usagePercentage = (currentUsage / totalAmount) * 100
 
   return (
     <>
-      <Box textAlign="center">
-        <Typography variant="h5">{new Date().getMonth() + 1}월 식대 사용 현황</Typography>
-        <Box
-          sx={{
-            width: '80%',
-            height: '30px',
-            backgroundColor: (theme) => theme.palette.background.default,
-            margin: '10px auto',
-            border: (theme) => `2px solid ${theme.palette.divider}`,
-          }}
-        >
-          <Box
-            sx={{
-              width: `${usagePercentage}%`,
-              height: '100%',
-              backgroundColor: (theme) => theme.palette.background.paper,
-              border: (theme) => `2px solid ${theme.palette.divider}`,
-            }}
-          />
-        </Box>
-        <Typography>
-          {currentUsage.toLocaleString()}원 / {totalAmount.toLocaleString()}원
-        </Typography>
-        <Typography sx={{ color: (theme) => theme.palette.text.primary, fontWeight: 'bold' }}>
-          남은 금액 : {remainingAmount.toLocaleString()}원
-        </Typography>
-      </Box>
+      <Typography variant="h5">{title}</Typography>
 
-      <Gauge
-        value={currentUsage}
-        valueMax={totalAmount}
-        startAngle={-100}
-        endAngle={100}
-        height={200}
-        sx={{
-          [`& .${gaugeClasses.valueText}`]: {
-            fontSize: 20,
-            transform: 'translate(0px, 0px)',
-            color: (theme) => theme.palette.text.primary,
-          },
-        }}
-        text={({ value, valueMax }) => `${value} / ${valueMax}`}
-      />
+      <Stack
+        direction="row"
+        spacing={2}
+        alignItems="center"
+        justifyContent="space-between"
+        sx={(theme) => ({
+          width: '50%',
+          maxWidth: '600px',
+          margin: '0 auto',
+          [theme.breakpoints.down('sm')]: { width: '90%' },
+        })}
+      >
+        <Stack sx={{ flexBasis: '70%' }}>
+          <Gauge
+            value={currentUsage}
+            valueMax={totalAmount}
+            startAngle={-100}
+            endAngle={100}
+            height={200}
+            sx={(theme) => ({
+              [`& .${gaugeClasses.valueText}`]: {
+                fontSize: 20,
+                transform: 'translate(0px, 0px)',
+                color: theme.palette.text.primary,
+              },
+            })}
+            text=""
+          />
+        </Stack>
+        <Stack sx={{ flexBasis: '30%', textAlign: 'right' }}>
+          <Typography>
+            {currentUsage.toLocaleString()}
+            {t('current.won')}
+          </Typography>
+          <Typography>
+            / {totalAmount.toLocaleString()}
+            {t('current.won')}
+          </Typography>
+        </Stack>
+      </Stack>
     </>
   )
 }
