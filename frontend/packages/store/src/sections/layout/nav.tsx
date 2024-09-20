9f18122039e46fe11132265eff0f722109adc6e5
황세웅 <0cookieboy0@gmail.com>
2024-09-20 15:11:21 +0900

    [#16] feat: 사이드바 drawer 설정

--- frontend/packages/store/src/sections/layout/nav.tsx
+++ frontend/packages/store/src/sections/layout/nav.tsx
@@ -9,11 +9,24 @@ import { useBoolean } from '@e201/utils'
 import tossLogo from '@/assets/img/toss-logo.jpg'
 import { useLocation, useNavigate } from 'react-router-dom'
 
-import { Box, Stack, alpha, Button, Collapse, Typography } from '@mui/material'
+import {
+  Box,
+  Stack,
+  alpha,
+  Button,
+  Collapse,
+  useTheme,
+  Typography,
+  useMediaQuery,
+} from '@mui/material'
 
 import { Link, Iconify, ScrollContainer } from '@e201/ui'
 
-export default function Nav() {
+interface IProps {
+  drawer?: boolean
+}
+
+export default function Nav({ drawer }: IProps) {
   const { t } = useTranslate('nav')
 
   const location = useLocation()
@@ -22,69 +35,77 @@ export default function Nav() {
     // logout
   }
 
+  const { breakpoints } = useTheme()
+  const invisible = useMediaQuery(breakpoints.down('md')) && !drawer
+
   return (
-    <ScrollContainer
+    <Stack
       sx={{
         width: 300,
         height: '100vh',
         flexShrink: 0,
         borderRight: (theme) => `1px solid ${theme.palette.divider}`,
+        display: invisible ? 'none' : 'flex',
       }}
     >
-      <Stack
-        mb={2}
-        top={0}
-        width={1}
-        height={76}
-        position="sticky"
-        zIndex={1}
-        justifyContent="center"
-        sx={{ bgcolor: (theme) => theme.palette.background.default }}
-      >
-        <Link
-          to={paths.main}
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <Box
-            component="img"
-            alt="logo"
-            src={tossLogo}
-            sx={{
-              height: 60,
-              objectFit: 'cover',
-            }}
-          />
-        </Link>
-      </Stack>
-      <Stack component="nav" py={1} px={2} spacing={3}>
-        {nav.map((group, i1) => (
-          <Stack key={i1} spacing={0.5} sx={{ color: (theme) => theme.palette.text.secondary }}>
-            <Typography variant="subtitle1" pl={2} pb={1}>
-              {t(group.title)}
-            </Typography>
-            {group.group.map((item, i2) => (
-              <NavItem key={i2} item={item} path={location.pathname} />
-            ))}
-          </Stack>
-        ))}
-        <Button variant="contained" color="error" onClick={logoutHandler}>
-          {t('logout')}
-        </Button>
-      </Stack>
-    </ScrollContainer>
+      <NavLogo />
+      <ScrollContainer sx={{ flex: 1, bgcolor: (theme) => theme.palette.background.default }}>
+        <Stack component="nav" py={1} px={2} spacing={3}>
+          {nav.map((group, i1) => (
+            <Stack key={i1} spacing={0.5} sx={{ color: (theme) => theme.palette.text.secondary }}>
+              <Typography variant="subtitle1" pl={2} pb={1}>
+                {t(group.title)}
+              </Typography>
+              {group.group.map((item, i2) => (
+                <NavItem key={i2} item={item} path={location.pathname} />
+              ))}
+            </Stack>
+          ))}
+          <Button variant="contained" color="error" onClick={logoutHandler}>
+            {t('logout')}
+          </Button>
+        </Stack>
+      </ScrollContainer>
+    </Stack>
   )
 }
 
-interface IProps {
+function NavLogo() {
+  return (
+    <Stack
+      height={76}
+      direction="row"
+      alignItems="center"
+      sx={{ bgcolor: (theme) => theme.palette.background.default }}
+    >
+      <Link
+        to={paths.main}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          component="img"
+          alt="logo"
+          src={tossLogo}
+          sx={{
+            height: 60,
+            objectFit: 'cover',
+          }}
+        />
+      </Link>
+    </Stack>
+  )
+}
+
+interface INavItemProps {
   item: INavItem
   depth?: number
   path: string
 }
 
-function NavItem({ item, depth = 0, path }: IProps) {
+function NavItem({ item, depth = 0, path }: INavItemProps) {
   const { t } = useTranslate('nav')
 
   const navigate = useNavigate()
