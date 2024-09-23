845efa46dd23e09c8fa1a71cabd909cd9e6bfa12
이준혁 <jhl221123@naver.com>
2024-09-23 15:46:29 +0900

    [#21] fix: 인증 로직 수정

--- backend/src/main/java/com/e201/api/controller/company/CompanyAuthController.java
+++ backend/src/main/java/com/e201/api/controller/company/CompanyAuthController.java
@@ -1,5 +1,6 @@
 package com.e201.api.controller.company;
 
+import static com.e201.global.security.auth.constant.AuthConstant.*;
 import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,7 @@ import com.e201.api.controller.company.request.manager.ManagerAuthRequest;
 import com.e201.api.service.company.CompanyService;
 import com.e201.api.service.company.EmployeeService;
 import com.e201.api.service.company.ManagerService;
-import com.e201.global.auth.response.AuthResponse;
+import com.e201.global.security.auth.dto.AuthInfo;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -28,22 +29,22 @@ public class CompanyAuthController {
 
 	@PostMapping("/companies/auth")
 	public ResponseEntity<Void> authCompany(@RequestBody CompanyAuthRequest request, HttpServletRequest httpRequest) {
-		AuthResponse authInfo = companyService.checkPassword(request);
-		httpRequest.getSession().setAttribute("authInfo", authInfo);
+		AuthInfo authInfo = companyService.checkPassword(request);
+		httpRequest.getSession().setAttribute(AUTH_INFO.name(), authInfo);
 		return ResponseEntity.status(CREATED).build();
 	}
 
 	@PostMapping("/companies/managers/auth")
 	public ResponseEntity<Void> authManager(@RequestBody ManagerAuthRequest request, HttpServletRequest httpRequest) {
-		AuthResponse authInfo = managerService.checkPassword(request);
-		httpRequest.getSession().setAttribute("authInfo", authInfo);
+		AuthInfo authInfo = managerService.checkPassword(request);
+		httpRequest.getSession().setAttribute(AUTH_INFO.name(), authInfo);
 		return ResponseEntity.status(CREATED).build();
 	}
 
 	@PostMapping("/companies/employees/auth")
 	public ResponseEntity<Void> authEmployee(@RequestBody EmployeeAuthRequest request, HttpServletRequest httpRequest) {
-		AuthResponse authInfo = employeeService.checkPassword(request);
-		httpRequest.getSession().setAttribute("authInfo", authInfo);
+		AuthInfo authInfo = employeeService.checkPassword(request);
+		httpRequest.getSession().setAttribute(AUTH_INFO.name(), authInfo);
 		return ResponseEntity.status(CREATED).build();
 	}
 }
