106aaec1e45361abb051649498fb6fce44fdd16d
김강진 <kkj1250@gmail.com>
2024-09-24 15:13:46 +0900

    [#17] feat: 세금계산서 File I/O 관련 기능 구현
    - Invoice Controller 작성
    - Invoice Service 작성

--- backend/src/main/java/com/e201/api/service/contract/InvoiceService.java
+++ backend/src/main/java/com/e201/api/service/contract/InvoiceService.java
@@ -1,10 +1,24 @@
 package com.e201.api.service.contract;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.e201.api.controller.contract.response.InvoiceCreateResponse;
+import com.e201.api.controller.contract.response.InvoiceDownloadResponse;
+import com.e201.domain.annotation.JtaTransactional;
+import com.e201.domain.entity.contract.Contract;
 import com.e201.domain.entity.contract.Invoice;
+import com.e201.domain.repository.contract.ContractRepository;
 import com.e201.domain.repository.contract.InvoiceRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,9 +28,72 @@ import lombok.RequiredArgsConstructor;
 public class InvoiceService {
 
 	private final InvoiceRepository invoiceRepository;
+	private final ContractRepository contractRepository;
 
-	public Invoice findDomain(UUID id){
+	@Value("${spring.servlet.multipart.location}")
+	private String path;
+
+	public Invoice findDomain(UUID id) {
 		return invoiceRepository.findById(id)
 			.orElseThrow(() -> new RuntimeException("not found exception"));
+	}
+
+	@JtaTransactional
+	public InvoiceCreateResponse create(MultipartFile uploadFile, String contractId) {
+		Contract contract = contractRepository.findById(UUID.fromString(contractId))
+			.orElseThrow(() -> new RuntimeException("not found exception"));
+
+		String res = fileUpload(uploadFile, contractId);
+		System.out.println(res);
+
+		Invoice invoice = Invoice.builder()
+			.contract(contract)
+			.imageUrl(res).build();
+
+		invoiceRepository.save(invoice);
+		return new InvoiceCreateResponse(UUID.fromString(contractId));
+	}
+
+	public InvoiceDownloadResponse find(String invoiceId) throws IOException {
+		Invoice invoice = invoiceRepository.findById(UUID.fromString(invoiceId))
+			.orElseThrow(() -> new RuntimeException("not found exception"));
+
+		Path invoiceFile = Paths.get(path + invoice.getImageUrl());
+
+		String contentType = Files.probeContentType(invoiceFile);
+		Resource resource = new InputStreamResource(Files.newInputStream(invoiceFile));
+
+		return new InvoiceDownloadResponse(resource, contentType, invoiceFile.getFileName().toString());
+	}
+
+	@JtaTransactional
+	public void delete(String invoiceId) {
+		Invoice invoice = invoiceRepository.findById(UUID.fromString(invoiceId))
+			.orElseThrow(() -> new RuntimeException("not found exception"));
+
+		invoice.delete();
+	}
+
+	private String fileUpload(MultipartFile uploadFile, String contractId) {
+		String folder = "invoice" + File.separator + contractId + File.separator;
+		File dir = new File(path + folder);
+		if (!dir.exists())
+			dir.mkdirs();
+
+		// 확장자 추출
+		String original = uploadFile.getOriginalFilename();
+		String ext = original.substring(original.lastIndexOf("."));
+
+		// 신규 파일 생성
+		String newFileName = folder + UUID.randomUUID().toString() + ext;
+		File newfile = new File(newFileName);
+
+		try {
+			uploadFile.transferTo(newfile);
+		} catch (IOException e) {
+			throw new RuntimeException("failed to upload file");
+		}
+
+		return newFileName;
 	}
 }
--- backend/src/main/java/com/e201/domain/repository/contract/ContractRepository.java
+++ backend/src/main/java/com/e201/domain/repository/contract/ContractRepository.java
@@ -4,8 +4,8 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.e201.domain.entity.company.Company;
 import com.e201.domain.entity.contract.Contract;
 
 public interface ContractRepository extends JpaRepository<Contract, UUID> {
+	
 }
