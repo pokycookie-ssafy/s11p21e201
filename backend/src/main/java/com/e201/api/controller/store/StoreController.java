a9dcb757ef4e55709af796c75ec9eb97189694f0
김재경 <dongji_11@naver.com>
2024-09-26 15:27:51 +0900

    [#29] feat: 식당 단건 정보 조회 기능 구현

--- backend/src/main/java/com/e201/api/controller/store/StoreController.java
+++ backend/src/main/java/com/e201/api/controller/store/StoreController.java
@@ -2,13 +2,18 @@ package com.e201.api.controller.store;
 
 import static org.springframework.http.HttpStatus.*;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.e201.api.controller.store.request.StoreCreateRequest;
 import com.e201.api.controller.store.response.StoreCreateResponse;
+import com.e201.api.controller.store.response.StoreFindResponse;
 import com.e201.api.service.store.StoreService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,5 +27,11 @@ public class StoreController {
 	public ResponseEntity<StoreCreateResponse> create(@RequestBody StoreCreateRequest storeCreateRequest){
 		StoreCreateResponse response = storeService.create(storeCreateRequest);
 		return ResponseEntity.status(CREATED).body(response);
+	}
+
+	@GetMapping("/stores/{id}")
+	public ResponseEntity<StoreFindResponse> findOne(@PathVariable UUID id){
+		StoreFindResponse response = storeService.findStore(id);
+		return ResponseEntity.status(OK).body(response);
 	}
 }
