5311f4e595ae34a931ca96baaecb8d6b69b92bdb
황세웅 <0cookieboy0@gmail.com>
2024-09-27 12:43:39 +0900

    [#23] feat: 메뉴 관리 페이지

--- frontend/packages/store/src/pages/management/menu-management-view.tsx
+++ frontend/packages/store/src/pages/management/menu-management-view.tsx
@@ -1,18 +1,18 @@
 import type { IMenu } from '@/types/menu'
-import type { GridColDef, GridRowParams } from '@mui/x-data-grid'
 
 import api from '@/configs/api'
 import axios from '@/configs/axios'
 import paths from '@/configs/paths'
 import { useTranslate } from '@/locales'
+import { useBoolean } from '@e201/utils'
 import { useMemo, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import { fNumber, useBoolean } from '@e201/utils'
 import { Breadcrumbs } from '@/components/breadcrumbs'
+import menuColumns from '@/sections/menu-management/menu-columns'
 import NewMenuModal from '@/sections/menu-management/new-menu-modal'
 
 import { DataGrid } from '@mui/x-data-grid'
-import { Box, Tab, Card, Tabs, Stack, Button, Tooltip, TextField, IconButton } from '@mui/material'
+import { Box, Tab, Card, Tabs, Stack, Button, TextField } from '@mui/material'
 
 import { Iconify, Typography } from '@e201/ui'
 
@@ -20,6 +20,7 @@ export default function MenuManagementView() {
   const { t } = useTranslate('menu-management')
 
   const [tab, setTab] = useState<string | null>(null)
+  const [search, setSearch] = useState<string>('')
 
   const newMenuModal = useBoolean()
 
@@ -28,47 +29,37 @@ export default function MenuManagementView() {
     return response.data
   }
 
-  const { data, isPending, isError } = useQuery({ queryKey: [api.menu.list], queryFn })
+  const { data: menus, isPending, isError } = useQuery({ queryKey: [api.menu.list], queryFn })
 
-  const columns: GridColDef[] = [
-    { field: 'name', headerName: t('field.name'), flex: 1, minWidth: 100 },
-    { field: 'category', headerName: t('field.category'), width: 100 },
-    {
-      field: 'price',
-      headerName: t('field.price'),
-      valueFormatter: (value: number) => `${fNumber(value)}${t('unit.won')}`,
-    },
-    {
-      field: 'action',
-      type: 'actions',
-      align: 'left',
-      getActions: (params: GridRowParams) => [
-        <Tooltip title={t('tooltip.edit')}>
-          <IconButton>
-            <Iconify icon="solar:pen-linear" />
-          </IconButton>
-        </Tooltip>,
-        <Tooltip title={t('tooltip.delete')}>
-          <IconButton color="error">
-            <Iconify icon="solar:trash-bin-minimalistic-2-linear" />
-          </IconButton>
-        </Tooltip>,
-      ],
-    },
-  ]
+  const columns = useMemo(() => menuColumns(t), [t])
+
+  const filteredMenus = useMemo<IMenu[]>(() => {
+    if (!menus) {
+      return []
+    }
+
+    let filtered = [...menus]
+    if (tab !== null) {
+      filtered = filtered.filter((menu) => menu.category === tab)
+    }
+    if (search.trim() !== '') {
+      filtered = filtered.filter((menu) => menu.name.includes(search.trim()))
+    }
+    return filtered
+  }, [menus, search, tab])
 
   const categories = useMemo(() => {
-    if (!data) {
+    if (!menus) {
       return []
     }
     const categorySet = new Set<string>()
-    data.forEach((menu) => {
+    menus.forEach((menu) => {
       if (menu.category) {
         categorySet.add(menu.category)
       }
     })
     return Array.from(categorySet)
-  }, [data])
+  }, [menus])
 
   if (isError) {
     return 'Error'
@@ -84,7 +75,7 @@ export default function MenuManagementView() {
             { title: t('breadcrumbs.menu_management') },
           ]}
           action={
-            <Button onClick={newMenuModal.onTrue}>
+            <Button onClick={newMenuModal.onTrue} color="secondary">
               <Iconify icon="ic:round-plus" />
               <Typography variant="subtitle2" pl={0.5}>
                 {t('button.create_menu')}
@@ -95,7 +86,12 @@ export default function MenuManagementView() {
 
         <Card>
           <Box px={2} sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}>
-            <Tabs value={tab} onChange={(_, v) => setTab(v)} variant="scrollable">
+            <Tabs
+              value={tab}
+              onChange={(_, v) => setTab(v)}
+              variant="scrollable"
+              sx={{ borderColor: 'red' }}
+            >
               <Tab label={t('label.all')} value={null} key={0} />
               {categories.map((category, i) => (
                 <Tab label={category} value={category} key={i + 1} />
@@ -107,43 +103,29 @@ export default function MenuManagementView() {
             direction="row"
             justifyContent="space-between"
             alignItems="center"
-            p={1}
+            p={2}
             sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
           >
-            <Stack width={1} direction="row" alignItems="center" spacing={1}>
-              <TextField size="small" label="search" fullWidth />
-            </Stack>
+            <TextField
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              size="small"
+              label={t('label.search')}
+              fullWidth
+            />
           </Stack>
 
           <DataGrid
             columns={columns}
-            rows={data}
+            rows={filteredMenus}
             checkboxSelection
             hideFooter
-            hideFooterPagination
-            disableColumnSorting
-            disableColumnFilter
-            disableColumnMenu
-            disableRowSelectionOnClick
             loading={isPending}
             slotProps={{
               noRowsOverlay: {},
               noResultsOverlay: {},
             }}
-            sx={{
-              height: 500,
-              '& .MuiDataGrid-columnSeparator': {
-                color: 'transparent',
-                ':hover': {
-                  color: (theme) => theme.palette.divider,
-                },
-              },
-              '& .MuiDataGrid-cell:focus-within': { outline: 'none' },
-              '& .MuiDataGrid-columnHeader:focus-within': { outline: 'none' },
-              '.MuiDataGrid-columnHeaders': {
-                borderBottom: (theme) => `1px solid ${theme.palette.divider}`,
-              },
-            }}
+            sx={{ height: 500 }}
           />
         </Card>
       </Box>
