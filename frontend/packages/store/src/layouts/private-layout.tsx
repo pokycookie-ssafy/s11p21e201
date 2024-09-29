0702de9bd4a9c52b0a262fb8b62ddbadcfd58491
황세웅 <0cookieboy0@gmail.com>
2024-09-29 22:27:35 +0900

    [#23] fix: route path 버그 해결

--- frontend/packages/store/src/layouts/private-layout.tsx
+++ frontend/packages/store/src/layouts/private-layout.tsx
@@ -2,7 +2,7 @@ import paths from '@/configs/paths'
 import { useAuthStore } from '@/stores'
 import { Outlet, Navigate } from 'react-router-dom'
 
-export default function PrivateRoute() {
+export default function PrivateLayout() {
   const { isLogin } = useAuthStore()
 
   return isLogin ? <Outlet /> : <Navigate to={paths.auth.signIn} replace />
