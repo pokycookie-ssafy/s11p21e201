79e51531da6816f546741af10f25e83964cef35b
kkj1250 <kkj1250@gmail.com>
2024-10-03 03:06:31 +0900

    [#66] refactor: Ocr Response field 업데이트
    - OCR Response DTO Field 수정

--- backend/src/main/java/com/e201/client/service/OcrService.java
+++ backend/src/main/java/com/e201/client/service/OcrService.java
@@ -1,8 +1,10 @@
 package com.e201.client.service;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -148,14 +150,13 @@ public class OcrService {
 		Result bizLicense = responseBody.getImages().getFirst().getBizLicense().getResult();
 
 		String taxType = bizLicense.getTaxType().getFirst().getText();
-		System.out.println(taxType);
-		String companyName;
+		String businessName;
 		switch (taxType) {
 			case "법인사업자" -> {
-				companyName = bizLicense.getCorpName().getFirst().getText();
+				businessName = bizLicense.getCorpName().getFirst().getText();
 			}
 			case "일반과세자", "간이과세자" -> {
-				companyName = bizLicense.getCompanyName().getFirst().getText();
+				businessName = bizLicense.getCompanyName().getFirst().getText();
 			}
 			default -> throw new IllegalStateException("Unexpected value: " + taxType);
 		}
@@ -213,11 +214,11 @@ public class OcrService {
 =======
 >>>>>>> 21808c9 ([#25] feat: OCR 관련 로직 보완)
 		String openDate = bizLicense.getOpenDate().getFirst().getText();
-		String bisType = bizLicense.getBisType().getFirst().getText();
-		String bisItem = bizLicense.getBisItem().getFirst().getText();
-		String bisAddress = bizLicense.getBisAddress().getFirst().getText();
+		String businessType = bizLicense.getBisType().getFirst().getText();
+		String address = bizLicense.getBisAddress().getFirst().getText();
 		String registerNumber= bizLicense.getRegisterNumber().getFirst().getText();
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -241,6 +242,9 @@ public class OcrService {
 =======
 		return new LicenseCreateResponse(repName, companyName, registerNumber, openDate, bisType, bisItem, bisAddress);
 >>>>>>> 21808c9 ([#25] feat: OCR 관련 로직 보완)
+=======
+		return new LicenseCreateResponse(repName, businessName, registerNumber, address, openDate, businessType);
+>>>>>>> 5e5ed54 ([#66] refactor: Ocr Response field 업데이트)
 	}
 
 	private String createRequestBody(MultipartFile file) {
