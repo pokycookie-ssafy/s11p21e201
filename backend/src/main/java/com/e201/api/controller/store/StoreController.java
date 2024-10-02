52637f9e8d7351f9a55400869c61afd250374dfa
김재경 <dongji_11@naver.com>
2024-10-02 15:28:30 +0900

    [#57] feat: store 회원가입 시 email 중복 여부 체크 기능 구현

--- backend/src/main/java/com/e201/api/controller/store/StoreController.java
+++ backend/src/main/java/com/e201/api/controller/store/StoreController.java
@@ -4,6 +4,8 @@ import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,7 @@ public class StoreController {
 
 	@PostMapping("/stores")
 	public ResponseEntity<StoreCreateResponse> create(@RequestBody StoreAndStoreInfoCreateRequest storeAndStoreInfoCreateRequest){
+		storeService.checkDuplication(storeAndStoreInfoCreateRequest.getEmail());
 		StoreCreateResponse response = storeService.create(storeAndStoreInfoCreateRequest);
 		return ResponseEntity.status(CREATED).body(response);
 	}
@@ -34,5 +37,4 @@ public class StoreController {
 		StoreDeleteResponse response = storeService.delete(authInfo.getId(), authInfo.getRoleType());
 		return ResponseEntity.status(NO_CONTENT).body(response);
 	}
-
 }
