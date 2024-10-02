70db6b686a3c8f6b04e24254ce4c937e477ccbdf
이준혁 <jhl221123@naver.com>
2024-10-02 17:36:46 +0900

    [#61] feat: 관리자, 직원 정보 목록 조회 기능

--- backend/src/main/java/com/e201/api/controller/company/EmployeeController.java
+++ backend/src/main/java/com/e201/api/controller/company/EmployeeController.java
@@ -2,13 +2,20 @@ package com.e201.api.controller.company;
 
 import static org.springframework.http.HttpStatus.*;
 
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.e201.api.controller.company.request.employee.EmployeeCreateRequest;
 import com.e201.api.controller.company.response.employee.EmployeeCreateResponse;
+import com.e201.api.controller.company.response.employee.EmployeeFindResponse;
 import com.e201.api.service.company.EmployeeService;
 import com.e201.global.security.auth.dto.AuthInfo;
 import com.e201.global.security.auth.resolver.Auth;
@@ -26,5 +33,12 @@ public class EmployeeController {
 		@RequestBody EmployeeCreateRequest request) {
 		EmployeeCreateResponse response = employeeService.create(request, authInfo.getId());
 		return ResponseEntity.status(CREATED).body(response);
+	}
+
+	@GetMapping("/companies/employees")
+	public ResponseEntity<Page<EmployeeFindResponse>> findPage(@RequestParam(required = false) UUID departmentId,
+		Pageable pageable) {
+		Page<EmployeeFindResponse> response = employeeService.findPage(departmentId, pageable);
+		return ResponseEntity.ok(response);
 	}
 }
