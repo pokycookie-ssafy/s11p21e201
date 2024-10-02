3de1515d171f02179a78541a0acb2bc2c0948724
황세웅 <0cookieboy0@gmail.com>
2024-10-02 12:49:07 +0900

    [#54] feat: 계약 해지

--- frontend/packages/company/src/pages/contract/contract-now-management-view.tsx
+++ frontend/packages/company/src/pages/contract/contract-now-management-view.tsx
@@ -1,35 +1,53 @@
-import type { IContract } from '@/types/contract'
-import type { GridColDef } from '@mui/x-data-grid'
+import type { IContract, IContractResponse } from '@/types/contract'
+import type { GridColDef, GridRowSelectionModel } from '@mui/x-data-grid'
 
 import dayjs from 'dayjs'
+import { toast } from 'sonner'
 import paths from '@/configs/paths'
 import axios from '@/configs/axios'
 import { useTranslate } from '@/locales'
 import { useMemo, useState } from 'react'
+import { m, useBoolean } from '@e201/utils'
 import { useNavigate } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
+import { DialogDelete } from '@/components/dialog'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 import { DataGrid } from '@mui/x-data-grid'
-import { Box, Card, Stack, Button, TextField, Typography } from '@mui/material'
+import {
+  Box,
+  Card,
+  Stack,
+  Button,
+  Tooltip,
+  Collapse,
+  TextField,
+  Typography,
+  IconButton,
+} from '@mui/material'
 
 import { Iconify, Breadcrumbs } from '@e201/ui'
 
 export default function ContractNowManagementView() {
-  const fetchContracts = async () => {
-    const response = await axios.get<IContract[]>('/companies/stores')
-    return response.data
-  }
-
   const navigate = useNavigate()
 
   const { t } = useTranslate('contract')
+
+  const [selected, setSelected] = useState<GridRowSelectionModel>([])
+  const [storeSearch, setStoreSearch] = useState<string>('')
+
+  const queryClient = useQueryClient()
+
+  const deleteAllConfirm = useBoolean()
+
+  const fetchContracts = async () => {
+    const response = await axios.get<IContractResponse[]>('/companies/stores')
+    return response.data
+  }
 
   const { data, isPending } = useQuery({
     queryKey: ['contract-stores'],
     queryFn: fetchContracts,
   })
-
-  const [storeSearch, setStoreSearch] = useState<string>('')
 
   const filteredData = useMemo(() => {
     if (!data) {
@@ -38,12 +56,18 @@ export default function ContractNowManagementView() {
 
     let filtered = [...data]
     if (storeSearch.trim() !== '') {
-      filtered = filtered.filter((contract) => contract.name.includes(storeSearch.trim()))
+      filtered = filtered.filter((contract) => contract.storeName.includes(storeSearch.trim()))
     }
     return filtered
   }, [data, storeSearch])
 
-  const columns: GridColDef[] = [
+  const deleteSubmitHandler = () => {
+    toast.success(t('toast.contract_cancel'))
+    queryClient.invalidateQueries({ queryKey: [''] })
+    deleteAllConfirm.onFalse()
+  }
+
+  const columns: GridColDef<IContractResponse>[] = [
     { field: 'name', headerName: t('restaurant_name'), flex: 1, minWidth: 100 },
     { field: 'phone', headerName: t('phone_number'), width: 180, resizable: false },
     { field: 'address', headerName: t('address'), width: 300 },
@@ -58,56 +82,96 @@ export default function ContractNowManagementView() {
   ]
 
   return (
-    <Box>
-      <Breadcrumbs
-        title={t('breadcrumbs.contract_management')}
-        routes={[
-          { title: t('breadcrumbs.management'), path: paths.management.contract.now },
-          { title: t('breadcrumbs.contract_management'), path: paths.management.contract.now },
-          { title: t('breadcrumbs.contract_management') },
-        ]}
-        action={
-          <Button color="secondary" onClick={() => navigate(paths.management.contract.new)}>
-            <Iconify icon="ic:round-plus" />
-            <Typography variant="subtitle2" pl={0.5}>
-              {t('add_contract')}
-            </Typography>
-          </Button>
-        }
-      />
-
-      <Card>
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          p={2}
-          sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-        >
-          <Stack width={1} direction="row" alignItems="center" spacing={1}>
-            <TextField
-              value={storeSearch}
-              onChange={(e) => setStoreSearch(e.target.value)}
-              size="small"
-              label="search"
-              fullWidth
-            />
-          </Stack>
-        </Stack>
-
-        <DataGrid
-          columns={columns}
-          rows={filteredData}
-          checkboxSelection
-          hideFooter
-          loading={isPending}
-          slotProps={{
-            noRowsOverlay: {},
-            noResultsOverlay: {},
-          }}
-          sx={{ height: 500 }}
+    <>
+      <Box>
+        <Breadcrumbs
+          title={t('breadcrumbs.contract_management')}
+          routes={[
+            { title: t('breadcrumbs.management'), path: paths.management.contract.now },
+            { title: t('breadcrumbs.contract_management'), path: paths.management.contract.now },
+            { title: t('breadcrumbs.contract_management') },
+          ]}
+          action={
+            <Button color="secondary" onClick={() => navigate(paths.management.contract.new)}>
+              <Iconify icon="ic:round-plus" />
+              <Typography variant="subtitle2" pl={0.5}>
+                {t('add_contract')}
+              </Typography>
+            </Button>
+          }
         />
-      </Card>
-    </Box>
+
+        <Card>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            p={2}
+            sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+          >
+            <Stack width={1} direction="row" alignItems="center" spacing={1}>
+              <TextField
+                value={storeSearch}
+                onChange={(e) => setStoreSearch(e.target.value)}
+                size="small"
+                label="search"
+                fullWidth
+              />
+            </Stack>
+          </Stack>
+
+          <Collapse in={selected.length > 0}>
+            <Stack
+              width={1}
+              height={57}
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              px={2}
+              py={1}
+              bgcolor="background.paper"
+              sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+            >
+              <Typography variant="subtitle2">
+                {m(t('label.selected'), [selected.length])}
+              </Typography>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <Tooltip title={t('tooltip.contract_cancel')} disableInteractive>
+                  <IconButton color="error" onClick={deleteAllConfirm.onTrue}>
+                    <Iconify icon="line-md:file-cancel" />
+                  </IconButton>
+                </Tooltip>
+              </Stack>
+            </Stack>
+          </Collapse>
+
+          <DataGrid
+            columns={columns}
+            rows={filteredData}
+            rowSelectionModel={selected}
+            onRowSelectionModelChange={setSelected}
+            disableMultipleRowSelection
+            checkboxSelection
+            hideFooter
+            loading={isPending}
+            slotProps={{
+              noRowsOverlay: {},
+              noResultsOverlay: {},
+            }}
+            sx={{ height: 500 }}
+          />
+        </Card>
+      </Box>
+
+      <DialogDelete
+        open={deleteAllConfirm.value}
+        onClose={deleteAllConfirm.onFalse}
+        onSubmit={deleteSubmitHandler}
+        title={t('dialog.cancel')}
+        content={m(t('dialog.cancel_content'), [
+          data?.find((e) => e.contractId === selected[0])?.companyName ?? '',
+        ])}
+      />
+    </>
   )
 }
--- frontend/packages/store/src/pages/management/contract-now-management-view.tsx
+++ frontend/packages/store/src/pages/management/contract-now-management-view.tsx
@@ -1,19 +1,21 @@
-import type { GridColDef } from '@mui/x-data-grid'
 import type { IContractResponse } from '@/types/contract'
+import type { GridColDef, GridRowSelectionModel } from '@mui/x-data-grid'
 
 import dayjs from 'dayjs'
+import { toast } from 'sonner'
 import api from '@/configs/api'
-import { m } from '@e201/utils'
 import paths from '@/configs/paths'
 import axios from '@/configs/axios'
 import { useTranslate } from '@/locales'
 import { useMemo, useState } from 'react'
+import { m, useBoolean } from '@e201/utils'
 import isBetween from 'dayjs/plugin/isBetween'
 import { useNavigate } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
+import { DialogDelete } from '@/components/dialog'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 import { DataGrid } from '@mui/x-data-grid'
-import { Box, Card, Stack, Button, TextField } from '@mui/material'
+import { Box, Card, Stack, Button, Tooltip, Collapse, TextField, IconButton } from '@mui/material'
 
 import { Iconify, Typography, Breadcrumbs } from '@e201/ui'
 
@@ -22,9 +24,14 @@ dayjs.extend(isBetween)
 export default function ContractNowManagementView() {
   const { t } = useTranslate('contract-management')
 
+  const [selected, setSelected] = useState<GridRowSelectionModel>([])
+  const [companySearch, setCompanySearch] = useState<string>('')
+
   const navigate = useNavigate()
 
-  const [companySearch, setCompanySearch] = useState<string>('')
+  const queryClient = useQueryClient()
+
+  const deleteAllConfirm = useBoolean()
 
   const queryFn = async () => {
     const response = await axios.get<IContractResponse[]>(api.contract.list)
@@ -44,6 +51,12 @@ export default function ContractNowManagementView() {
     }
     return filtered
   }, [data, companySearch])
+
+  const deleteSubmitHandler = () => {
+    toast.success(t('toast.contract_cancel'))
+    queryClient.invalidateQueries({ queryKey: [api.contract.list] })
+    deleteAllConfirm.onFalse()
+  }
 
   const columns: GridColDef[] = [
     { field: 'companyName', headerName: t('field.company_name'), flex: 1, minWidth: 100 },
@@ -66,55 +79,96 @@ export default function ContractNowManagementView() {
   ]
 
   return (
-    <Box>
-      <Breadcrumbs
-        title={t('breadcrumbs.contract_management')}
-        routes={[
-          { title: t('breadcrumbs.management'), path: paths.management.menu },
-          { title: t('breadcrumbs.contract_management'), path: paths.management.contract.now },
-          { title: t('breadcrumbs.contract_management') },
-        ]}
-        action={
-          <Button color="secondary" onClick={() => navigate(paths.management.contract.new)}>
-            <Iconify icon="ic:round-plus" />
-            <Typography variant="subtitle2" pl={0.5}>
-              {t('button.create_contract')}
-            </Typography>
-          </Button>
-        }
-      />
-
-      <Card>
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          p={2}
-          spacing={1}
-          sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-        >
-          <TextField
-            value={companySearch}
-            onChange={(e) => setCompanySearch(e.target.value)}
-            size="small"
-            label={t('label.company_search')}
-            fullWidth
-          />
-        </Stack>
-        <DataGrid
-          columns={columns}
-          getRowId={(row) => row.contractId}
-          rows={filteredData}
-          checkboxSelection
-          hideFooter
-          loading={isPending}
-          slotProps={{
-            noRowsOverlay: {},
-            noResultsOverlay: {},
-          }}
-          sx={{ height: 500 }}
+    <>
+      <Box>
+        <Breadcrumbs
+          title={t('breadcrumbs.contract_management')}
+          routes={[
+            { title: t('breadcrumbs.management'), path: paths.management.menu },
+            { title: t('breadcrumbs.contract_management'), path: paths.management.contract.now },
+            { title: t('breadcrumbs.contract_management') },
+          ]}
+          action={
+            <Button color="secondary" onClick={() => navigate(paths.management.contract.new)}>
+              <Iconify icon="ic:round-plus" />
+              <Typography variant="subtitle2" pl={0.5}>
+                {t('button.create_contract')}
+              </Typography>
+            </Button>
+          }
         />
-      </Card>
-    </Box>
+
+        <Card>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            p={2}
+            spacing={1}
+            sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+          >
+            <TextField
+              value={companySearch}
+              onChange={(e) => setCompanySearch(e.target.value)}
+              size="small"
+              label={t('label.company_search')}
+              fullWidth
+            />
+          </Stack>
+
+          <Collapse in={selected.length > 0}>
+            <Stack
+              width={1}
+              height={57}
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              px={2}
+              py={1}
+              bgcolor="background.paper"
+              sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+            >
+              <Typography variant="subtitle2">
+                {m(t('label.selected'), [selected.length])}
+              </Typography>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <Tooltip title={t('tooltip.contract_cancel')} disableInteractive>
+                  <IconButton color="error" onClick={deleteAllConfirm.onTrue}>
+                    <Iconify icon="line-md:file-cancel" />
+                  </IconButton>
+                </Tooltip>
+              </Stack>
+            </Stack>
+          </Collapse>
+
+          <DataGrid
+            columns={columns}
+            getRowId={(row) => row.contractId}
+            rows={filteredData}
+            rowSelectionModel={selected}
+            onRowSelectionModelChange={setSelected}
+            disableMultipleRowSelection
+            checkboxSelection
+            hideFooter
+            loading={isPending}
+            slotProps={{
+              noRowsOverlay: {},
+              noResultsOverlay: {},
+            }}
+            sx={{ height: 500 }}
+          />
+        </Card>
+      </Box>
+
+      <DialogDelete
+        open={deleteAllConfirm.value}
+        onClose={deleteAllConfirm.onFalse}
+        onSubmit={deleteSubmitHandler}
+        title={t('dialog.cancel')}
+        content={m(t('dialog.cancel_content'), [
+          data?.find((e) => e.contractId === selected[0])?.companyName ?? '',
+        ])}
+      />
+    </>
   )
 }
