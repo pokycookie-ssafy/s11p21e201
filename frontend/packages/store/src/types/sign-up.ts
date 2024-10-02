bb88e35379ad97d45d6043bb90ff9f2b824e8648
황세웅 <0cookieboy0@gmail.com>
2024-10-02 18:01:22 +0900

    [#54] feat: 회원가입 백엔드 연결

--- frontend/packages/store/src/types/sign-up.ts
+++ frontend/packages/store/src/types/sign-up.ts
@@ -12,4 +12,17 @@ interface IBankResponse {
   bankName: string
 }
 
-export type { IBankResponse, ISignUpResponse }
+interface ISignUpRequest {
+  email: string
+  password: string
+  passwordConfirm: string
+  phone: string
+  businessName: string
+  repName: string
+  address: string
+  registerNumber: string
+  businessType: string
+  openDate: Date
+}
+
+export type { IBankResponse, ISignUpRequest, ISignUpResponse }
