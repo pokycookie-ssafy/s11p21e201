644a45c9fa36cabbc84c865c2f2a7a13e0bf2e5e
김유리 <yuurrriiii@naver.com>
2024-10-03 17:18:28 +0900

    [#53] feat: 마이크로사이트 1차 완성

---
+++ frontend/packages/microsite/src/sections/layout/header.tsx
@@ -0,0 +1,50 @@
+import tossLogo from '@/assets/img/toss-logo.jpg'
+import { ModeButton, LocaleButton } from '@/components/setting-button'
+
+import { Box, alpha, Stack } from '@mui/material'
+
+import { Link } from '@e201/ui'
+
+interface IProps {
+  logo?: boolean
+}
+
+export default function Header({ logo }: IProps) {
+  // const { breakpoints } = useTheme()
+  // const visible = useMediaQuery(breakpoints.down('md'))
+
+  return (
+    <Stack
+      component="header"
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      position="fixed"
+      sx={{
+        bgcolor: (theme) => alpha(theme.palette.background.default, 0.7),
+        backdropFilter: 'blur(4px)',
+        width: '100%',
+        zIndex: 2,
+        px: 0,
+        py: 1,
+      }}
+    >
+      <Link to="/" sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box
+          component="img"
+          alt="logo"
+          src={tossLogo}
+          sx={{
+            width: 120,
+            objectFit: 'cover',
+          }}
+        />
+      </Link>
+
+      <Stack direction="row" justifyContent="flex-end">
+        <ModeButton />
+        <LocaleButton />
+      </Stack>
+    </Stack>
+  )
+}
