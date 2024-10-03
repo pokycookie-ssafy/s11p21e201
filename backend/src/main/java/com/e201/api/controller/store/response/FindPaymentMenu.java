95763316c11d760ff049ed998a6dc66e35ada386
김재경 <dongji_11@naver.com>
2024-10-04 00:24:32 +0900

    [#77] fix: merge 충돌 해결

---
+++ backend/src/main/java/com/e201/api/controller/store/response/FindPaymentMenu.java
@@ -0,0 +1,18 @@
+package com.e201.api.controller.store.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class FindPaymentMenu {
+	private String name;
+	private int price;
+
+	@Builder
+	private FindPaymentMenu(String name, int price) {
+		this.name=name;
+		this.price=price;
+	}
+}
