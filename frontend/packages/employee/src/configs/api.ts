4472c8acc7deea41a1dba2edced0dac23347d8b2
황세웅 <0cookieboy0@gmail.com>
2024-10-05 16:40:40 +0900

    [#54] fix: employee 버그 수정

--- frontend/packages/employee/src/configs/api.ts
+++ frontend/packages/employee/src/configs/api.ts
@@ -1,6 +1,8 @@
 export const BASE_URL = 'https://sanedaejangbu.site/api'
 
 const api = {
+  qr: '/employees/qr',
+
   employeeMeal: (id: string) => `/employees/${id}/meals`, // 식대 사용 현황 및 월 식대 조회 API
   storesList: '/employees/stores', // 제휴 식당 리스트 조회 API
   validationId: '/employees/qr', // QR 코드 uuid POST API
